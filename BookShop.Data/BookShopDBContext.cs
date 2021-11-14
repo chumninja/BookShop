89968279ed471a861a42e24d0fff1096e8e06251
21AK22-COM\Windows 10 <thuongdt1002@gmail.com>
2021-11-14 23:56:31 +0700

    commit book data base

---
+++ BookShop.Data/BookShopDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BookShop.Data
+{
+    public class BookShopDBContext : DBContext
+    {
+        public BookShopDBContext() : base("BookShopConnection")//key connection been app config
+        {
+            this.Configuration.LazyLoadingEnabled = false;// khi ma ta include bang cha thi no se ko tu include ra bang con
+        }
+        public DBset<Menu> meunu;
+    }
+}
