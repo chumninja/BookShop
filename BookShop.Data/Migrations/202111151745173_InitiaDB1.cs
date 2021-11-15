5c3fbe2dd8425f42473ccd517080d12752b88304
21AK22-COM\Windows 10 <thuongdt1002@gmail.com>
2021-11-16 00:53:27 +0700

    commit more service

---
+++ BookShop.Data/Migrations/202111151745173_InitiaDB1.cs
@@ -0,0 +1,30 @@
+﻿namespace BookShop.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitiaDB1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.PostCategories", "MetaDescription", c => c.String(maxLength: 256));
+            AddColumn("dbo.PostCategories", "MetaKeyWord", c => c.String(maxLength: 256));
+            AddColumn("dbo.PostCategories", "CreateBy", c => c.String(maxLength: 256));
+            AddColumn("dbo.PostCategories", "CreateDate", c => c.DateTime());
+            AddColumn("dbo.PostCategories", "UpdateBy", c => c.String(maxLength: 256));
+            AddColumn("dbo.PostCategories", "UpdateDate", c => c.DateTime());
+            AddColumn("dbo.PostCategories", "Status", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.PostCategories", "Status");
+            DropColumn("dbo.PostCategories", "UpdateDate");
+            DropColumn("dbo.PostCategories", "UpdateBy");
+            DropColumn("dbo.PostCategories", "CreateDate");
+            DropColumn("dbo.PostCategories", "CreateBy");
+            DropColumn("dbo.PostCategories", "MetaKeyWord");
+            DropColumn("dbo.PostCategories", "MetaDescription");
+        }
+    }
+}
