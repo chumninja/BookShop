4898520420669d138597c5b61e09a1e51d973a55
21AK22-COM\Windows 10 <thuongdt1002@gmail.com>
2021-11-24 01:17:39 +0700

    bai 37 tit ngòi

--- BookShop.Web/app/app.js
+++ BookShop.Web/app/app.js
@@ -3,14 +3,20 @@
 (function () {
     angular.module('bookshop', ['bookshop.products', 'bookshop.product_categories', 'bookshop.common']).config(config);
 
-    config.$inject =['$stateProvider','$urlRouterProvider']
+    config.$inject = ['$stateProvider', '$urlRouterProvider']
     function config($stateProvider, $urlRouterProvider) {
-        $stateProvider.state('home', {
-            url: "/admin",//url thực tế ở trên trang
-            templateUrl: "/app/components/home/homeView.html",//gọi đến template đã tách
-            controller: "homeController"// vao đc homeView.html nó tự động nhận đc homeController , 
-            //nhưng cần khai báo 1 file có tên zi
-        });
+        $stateProvider
+           .state('login', {
+               url: "/login",
+               templateUrl: "/app/components/LoginAdmin/loginView.html",
+               controller: "loginController"
+           })
+           .state('home', {
+               url: "/admin",
+               
+               templateUrl: "/app/components/home/homeView.html",
+               controller: "homeController"
+           });
         $urlRouterProvider.otherwise('/admin');
         //Ngược lại nếu ko có url route nào nó tự động trả về admin.
     }
